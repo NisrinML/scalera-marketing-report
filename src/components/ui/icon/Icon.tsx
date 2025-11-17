72b8d082d473707ef8dc551659d929baf829e385
NisrinMl <www.niso2001@gmail.com>
2025-11-17 11:11:05 +0300

    initial commit

---
+++ src/components/ui/icon/Icon.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { lazy, Suspense } from "react";
+type IconProps = {
+  name: string;
+  w?: string;
+  h?: string;
+  c?: string;
+  cls?: string;
+  m?: string;
+  [key: string]: any; 
+};
+
+const icons: Record<string, React.LazyExoticComponent<React.ComponentType<any>>> = {
+  //BurgerMenu: lazy(() => import("../icons/BurgerMenu")),
+
+  
+};
+
+const Icon: React.FC<IconProps> = ({
+  name,
+  w = "30",
+  h = "30",
+  c = "dark",
+  cls = "",
+  m,
+  ...rest
+}) => {
+  const key = name ? name.charAt(0).toUpperCase() + name.slice(1) : "";
+  if (!icons[key]) return null;
+  const Component = icons[key];
+
+  return (
+    <Suspense fallback={"loading"}>
+      <span
+        className={` ${cls} `}
+        style={{
+          minWidth: `${w}px`,
+          minHeight: `${h || w}px`,
+          margin: m,
+        }}
+        {...rest}
+      >
+        <Component style={{ width: w, height: h || w }} />
+      </span>
+    </Suspense>
+  );
+};
+
+export default Icon;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { MantineProvider } from '@mantine/core';
+import AppRouter from './router/index.tsx'
+import './index.css'
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <MantineProvider defaultColorScheme="light">
+       <AppRouter />
+    </MantineProvider>
+  </StrictMode>,
+)
